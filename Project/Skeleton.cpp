13dae6b90a8bfb9218351a92d7411af132938508
Jongsang Yoo <jamin0513@gmail.com>
2022-12-06 22:20:02 +0900

    update readme

--- Project/Skeleton.cpp
+++ Project/Skeleton.cpp
@@ -7,6 +7,8 @@ void cartsia2cylinder(double x, double y, double _z);
 double findTheta2(double r, double z);
 double findTheta1(double r, double z, double th2);
 void findInverseKinematic(double x, double y, double z);
+void potRead();
+
 double cartsianCord[3];
 double r, pi, z;
 double motorDeg[3];
@@ -16,14 +18,18 @@ int main(){
     findInverseKinematic(cartsianCord[0], cartsianCord[1], cartsianCord[2]);
     for (size_t i = 0; i < 3; i++)
         printf("%lf\t", motorDeg[i]);
-    /*
-    cartsia2cylinder(cartsianCord[0], cartsianCord[1], cartsianCord[2]);
-    printf("\nTrans cord: %lf\t%lf\t%lf\n",r,pi,z);
-    double theta2 = findTheta2(r,z);
-    double theta1 = findTheta1(r,z,theta2);
-    printf("Theta : %lf \t %lf\n",theta2, theta1);
-    */
     return 0;
+}
+
+void potRead(){
+    int val[3];
+    val[0] = analogRead(A0);
+    val[1] = analogRead(A0);
+    val[2] = analogRead(A0);
+    
+    for (int i = 0; i < 3; i++) {
+        motorDeg[i] = map(val[i], 0, 1024, 0, 360);
+    }    
 }
 
 void findInverseKinematic(double x, double y, double z){
